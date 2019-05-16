72465f741188d668e8beaec4e30ec24c1869b7e9
39395936+Parzival-X@users.noreply.github.com <39395936+Parzival-X@users.noreply.github.com>
2019-05-15 22:18:31 -0700

    Fixed the URLs file.

--- myblog/urls.py
+++ myblog/urls.py
@@ -4,8 +4,6 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 
 urlpatterns = [
-    path('', include('myblog.urls')),
-    path('admin/', admin.site.urls),
-    path('login/', LoginView.as_view(template_name='login.html'), name="login"),
-    path('logout/', LogoutView.as_view(next_page='/'), name="logout"),
+    path('', list_view, name="blog_index"),
+    path('posts/<int:post_id>/', detail_view, name="blog_detail"),
 ]
